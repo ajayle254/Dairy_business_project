bb3adcf52359f120de4de8607079ea1d9823940e
ajayle254 <jumaallanie@gmail.com>
2021-03-13 23:51:14 +0300

    add the above files

--- script.js
+++ script.js
@@ -21,3 +21,14 @@ var sheds = [
         averageProduction: 11
     }
 ];
+var numberOfSheds = sheds.length;
+var months = {January: 31, February:29, March:31, April:30, May:31, June:30, July:31, August:31, September:30, October:31, November:30, December:31};
+function totalProduction(verbose){
+total_production = 0;
+index = 0;
+while(index < numberOfSheds){
+    total_production += sheds[index].numberOfCows * sheds[index].averageProd;
+    shed_production = sheds[index].numberOfCows * sheds[index].averageProd;
+    if (verbose === true){
+
+    }
