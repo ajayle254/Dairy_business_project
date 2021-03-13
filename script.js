4e97b983e1932c1e1e1bbdd46e19159d4a2fc8a8
ajayle254 <jumaallanie@gmail.com>
2021-03-13 23:13:01 +0300

    add the above files

---
+++ script.js
@@ -0,0 +1,23 @@
+var sheds = [
+    {
+        name: "Shed A",
+        numberOfCows: 34,
+        averageProduction: 15
+    },
+    {
+        name: "Shed B",
+        numberOfCows: 11,
+        averageProduction: 28
+    },
+    {
+        name: "Shed C",
+        numberOfCows: 27,
+        averageProduction: 18
+
+    },
+    {
+        name: "Shed D",
+        numberOfCows: 52,
+        averageProduction: 11
+    }
+];
