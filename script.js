48c47c91d99f58203ed229a25867e171216b426c
ajayle254 <jumaallanie@gmail.com>
2021-03-15 08:42:03 +0300

    add the above file

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var sheds = [
     }
 ];
 var numberOfSheds = sheds.length;
-var months = {January: 31, February:29, March:31, April:30, May:31, June:30, July:31, August:31, September:30, October:31, November:30, December:31};
+var months = {January: 31, February:28, March:31, April:30, May:31, June:30, July:31, August:31, September:30, October:31, November:30, December:31};
 function totalProduction(verbose){
 total_production = 0;
 index = 0;
